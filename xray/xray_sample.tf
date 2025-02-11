4977a8f1731f5e4d4c913db64861d4ee58727da7
dhanush07 <dhanush.mohanvm@gmail.com>
2025-02-12 02:05:41 +0530

    Terraform Sample

--- xray/xray_sample.tf
+++ xray/xray_sample.tf
@@ -27,11 +27,7 @@ provider "xray" {
   url          = var.artifactory_url
   access_token = var.artifactory_token
 }
-# Take the json in https://git.jfrog.info/projects/PROFS/repos/cli_samples/browse/lab-3/security-policy.json 
-# and remove the double quotes on keys using https://csvjson.com/json_beautifier ( check the  "No quotes" + "on keys" )
-# Then  convert to HCL using https://www.hcl2json.com/  or https://www.convertsimple.com/convert-json-to-hcl/   
-#https://discuss.hashicorp.com/t/inconsistent-syntaxing/7398/3
-#https://github.com/kvz/json2hcl
+
 
 resource "xray_security_policy" "security1" {
   description = "block high risk vulnerabilities"
@@ -81,7 +77,6 @@ resource "xray_security_policy" "security1" {
   type = "security"
 }
 
-# https://github.com/jfrog/SwampUp2022/blob/main/SUP003-Intro_to_DevSecOps_with_JFrog_Xray/scripts/json/lab1-prod-sec-policy.json
 
 resource "xray_security_policy" "Prod-Security-Policy" {
   description = "This is a Security Policy for production Repos and Builds"
@@ -110,7 +105,7 @@ resource "xray_security_policy" "Prod-Security-Policy" {
   }
 }
 
-# https://github.com/jfrog/SwampUp2022/blob/main/SUP003-Intro_to_DevSecOps_with_JFrog_Xray/scripts/json/lab1-prod-lic-policy.json
+
 resource "xray_license_policy" "Prod-License-Policy" {
   name        = "Prod-License-Policy"
   description = "This is a License Policy for Production Repos and Builds"
@@ -142,55 +137,23 @@ resource "xray_license_policy" "Prod-License-Policy" {
   }
 }
 
-# https://github.com/jfrog/SwampUp2022/blob/main/SUP003-Intro_to_DevSecOps_with_JFrog_Xray/scripts/json/lab2-prod-watch.json
-/*=======
- Before creating the watch make sure you have repos  with "xray_index  = true" so that the requirement for the watch_resource
- "type = "all-repos" is satisfied. 
-
-But for the builds there is  no terraform directive to add or update builds so they get indexed by xray 
-
- You need to use the [Add Builds to Indexing Configuration](https://www.jfrog.com/confluence/display/JFROG/Xray+REST+API#XrayRESTAPI-AddBuildstoIndexingConfiguration)
- or [Update Builds Indexing Configuration](https://www.jfrog.com/confluence/display/JFROG/Xray+REST+API#XrayRESTAPI-UpdateBuildsIndexingConfiguration) REST APIs or  
- index the builds using the UI in the "Xray > Settings>General>Indexed Resources"
- ==========*/
-
-resource "xray_watch" "Prod-Watch" {
+resource "xray_watch" "Prod-Watchs" {
   name        = "Prod-Watch"
-  description = "This is a watch created for Production Repos and Builds"
+  description = "Watch for all repositories, matching the filter"
   active      = true
+  project_key = "bmwtest01"
+
   watch_resource {
-    type       = "repository"
-    bin_mgr_id = "default"
-    name       = "s003-libs-release-local"
-    repo_type  = "local"
+    type = "all-repos"
 
     filter {
       type  = "regex"
       value = ".*"
     }
   }
-  watch_resource {
-    type       = "build"
-    bin_mgr_id = "default"
-    name       = "swampup22_s003_mvn_pipeline"
-
-  }
-  watch_resource {
-    type       = "build"
-    bin_mgr_id = "default"
-    name       = "swampup22_s003_npm_pipeline"
-
-  }
-  assigned_policy {
+    assigned_policy {
     name = xray_security_policy.Prod-Security-Policy.name
     type = "security"
   }
-
-  assigned_policy {
-    name = xray_license_policy.Prod-License-Policy.name
-    type = "license"
-  }
-
-  watch_recipients = []
 }
 
